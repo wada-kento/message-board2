1700720792c23c0be54b0edbc0133e4e43dafd1e
“wadakento” <k.wada@axisem.xyz>
2020-10-03 10:45:32 +0900

    add messages index

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -19,23 +16,36 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/', function(req, res) {
+    res.redirect('/messages');
+});
+
+const db = require('./models/index');
+
+app.get('/messages', function(req, res) {
+    db.message.findAll(req.params.id).then(function(results) {
+        res.render('index.ejs', { messages: results })
+    });
+});
+
+app.post('/messages', function(req, res) {
+
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
